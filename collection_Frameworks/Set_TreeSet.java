d46a9e4213ab1a7f5f30e341286aa74cef4480f7
collision-hp <champatisatyabrata26@gmail.com>
2025-09-07 20:57:54 +0530

    Minor fixes

--- collection_Frameworks/Set_TreeSet.java
+++ collection_Frameworks/Set_TreeSet.java
@@ -4,13 +4,13 @@ public class Set_TreeSet {
     public static void main(String[] args) {
         // to arrange all the elements in sorted manner along with set properties
         // binary search tree is implemented
-        Set<Integer> hashset=new TreeSet<>();
-        hashset.add(32);
-        hashset.add(67);
-        hashset.add(32);
-        hashset.add(12);
-        hashset.add(65);
+        Set<Integer> treeset=new TreeSet<>();
+        treeset.add(32);
+        treeset.add(67);
+        treeset.add(32);
+        treeset.add(12);
+        treeset.add(65);
 
-        System.out.println(hashset);
+        System.out.println(treeset);
     }
 }
