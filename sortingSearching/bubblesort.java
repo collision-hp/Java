15d0d79be4c38981c058406cf53d221bd90c530e
collision-hp <champatisatyabrata26@gmail.com>
2025-10-23 23:06:12 +0530

    Minor fixes

--- sortingSearching/bubblesort.java
+++ sortingSearching/bubblesort.java
@@ -36,10 +36,10 @@ public class bubblesort {
         }
         for(int i=0;i<n-1;i++){
             if(arr[i]>arr[i+1]){
-                swap(arr,i,i+1);
+                swap(arr, i, i+1);
             }
         }
-        bubblerecursive(arr, n-1);   
+        bubblerecursive(arr, n-1);
     }
 
     public static void printarr(int arr[]){
